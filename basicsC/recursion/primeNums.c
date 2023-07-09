f27662e087626d47e2d15d7a6f0b68e003a65b94
francewill <franceureta7@gmail.com>
2023-07-09 18:04:33 +0800

    docs: add documentation

--- basicsC/recursion/primeNums.c
+++ basicsC/recursion/primeNums.c
@@ -7,17 +7,18 @@ int main()
     printf("\nWelcome!");
     while (1)
     {
-        printf("\nEnter a positive number: ");
+        printf("\nEnter a positive number: ");  // ask for user input
         scanf("%d", &userInput);
-        if (userInput < 2)
+        if (userInput < 2)  // If less than 2 then automatically give a prompt
         {
             printf("\nNo prime number values present.");
         }
         else
         {
             printf("The prime numbers from 1 to %d are:", userInput);
-            for (int startCounter = 2; startCounter <= userInput; startCounter++)
+            for (int startCounter = 2; startCounter <= userInput; startCounter++)  // loop for checking the prime numbers from 1 to user input
             {
+                // print the prime numbers
                 if(primeCheck(2,startCounter)==0){
                        printf(" %d", startCounter);
                 }
@@ -42,8 +43,8 @@ int main()
 }
 
 int primeCheck(int divisor, int num){
-    if(divisor==num) return 0;
-    else if(num % divisor==0) return 1;
-    else primeCheck(divisor+1,num);
+    if(divisor==num) return 0;  // base case returning 0 means it's a prime numebr
+    else if(num % divisor==0) return 1; // base case returning 1 means it's a composite number
+    else primeCheck(divisor+1,num);  // recursive case just add 1 in the divisor if the arguments above is not satisfied. 
 
 }
