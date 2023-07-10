3c92a5d42f2606078096622166474e101ff4bc06
francewill <franceureta7@gmail.com>
2023-07-10 22:03:56 +0800

    feat: add get input function using pass by reference

--- basicsC/top3prime/topPrime.c
+++ basicsC/top3prime/topPrime.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 int primeCheck(int divisor, int num);
+void getInput(int *x, int *y);
 
 int main(){
+    int x, y, largest1, largest2, largest3;
+    getInput(&x, &y);
+  
+
+
     return 0;
 }
 
@@ -9,4 +15,11 @@ int primeCheck(int divisor, int num){
     if(divisor == num) return 0; // prime number
     else if(num % divisor==0) return 1; // composite number
     else primeCheck(divisor+1,num);
+}
+
+void getInput(int *x, int *y){
+    printf("\nEnter a value for x: ");
+    scanf("%d", x);
+    printf("\nEnter a value for y: ");
+    scanf("%d", y);   
 }
