d3293fadd409601c8725d7be803cb0f7fc921834
francewill <franceureta7@gmail.com>
2023-07-10 19:36:26 +0800

    feat: add the recursive function

--- basicsC/top3prime/topPrime.c
+++ basicsC/top3prime/topPrime.c
@@ -1,4 +1,12 @@
 #include<stdio.h>
+int primeCheck(int divisor, int num);
+
 int main(){
     return 0;
+}
+
+int primeCheck(int divisor, int num){
+    if(divisor == num) return 0; // prime number
+    else if(num % divisor==0) return 1; // composite number
+    else primeCheck(divisor+1,num);
 }
