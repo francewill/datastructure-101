af4af93e549c3602f82206847ac611661d9475ca
francewill <franceureta7@gmail.com>
2023-07-13 23:04:51 +0800

    chore: create structure palylist and song

--- basicsC/playlist/playlist.c
+++ basicsC/playlist/playlist.c
@@ -1,6 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <string.h>
 
 int main(){
+    typedef struct{
+         char songTitle[30];
+         char artist[30];
+         char album[30];
+    } song;
+    
+    typedef struct{
+        char playlistName[30];
+        int songCount;
+        song arraySong[10];
+
+    } playlist;
     return 0;
 }
