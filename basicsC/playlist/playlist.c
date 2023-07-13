1d382ed15727df699601e89eb5d1a3ab1bf01196
francewill <franceureta7@gmail.com>
2023-07-13 23:40:56 +0800

    fix: structure make it global

--- basicsC/playlist/playlist.c
+++ basicsC/playlist/playlist.c
@@ -1,56 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+typedef struct
+{
+    char songTitle[30];
+    char artist[30];
+    char album[30];
+} song;
+
+typedef struct
+{
+    char playlistName[30];
+    int songCount;
+    song arraySong[10];
+
+} playlist;
 
 char menu();
+void addPlaylist(playlist *set, int index);
 
 int main()
 {
-    typedef struct
-    {
-        char songTitle[30];
-        char artist[30];
-        char album[30];
-    } song;
-
-    typedef struct
-    {
-        char playlistName[30];
-        int songCount;
-        song arraySong[10];
-
-    } playlist;
+    playlist setOfPlaylist[10];
+    int countPlaylist = 0;
 
     while (1)
     {
         char choice = menu();
         switch (choice)
         {
-            case '1':
-                printf("\nYou choose 1");
-                break;
-            case '2':
-                printf("\nYou choose 2");
-                break;
-            case '3':
-                printf("\nYou choose 3");
-                break;
-            case '4':
-                printf("\nYou choose 4");
-                break;
-            case '5':
-                printf("\nYou choose 5");
-                break;
-            case '6':
-                printf("\nYou choose 6");
-                break;
-            default:
-                printf("\nInvalid input!");
+        case '1':
+            printf("\nEnter your playlist name: ");
+
+            break;
+        case '2':
+            printf("\nYou choose 2");
+            break;
+        case '3':
+            printf("\nYou choose 3");
+            break;
+        case '4':
+            printf("\nYou choose 4");
+            break;
+        case '5':
+            printf("\nYou choose 5");
+            break;
+        case '6':
+            printf("\nThank you and goodbye!\n");
+            break;
+        default:
+            printf("\nInvalid input!\n");
         }
     }
 
     return 0;
 }
+
 char menu()
 {
     char choice;
@@ -64,4 +69,9 @@ char menu()
     printf("\n\nEnter your choice: ");
     scanf(" %c", &choice);
     return choice;
+}
+
+void addPlaylist(playlist *set, int index)
+{
+    
 }
