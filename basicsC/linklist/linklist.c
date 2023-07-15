1bdf66fab79bda6b25110c568e5a624cdf9bcb14
francewill <franceureta7@gmail.com>
2023-07-15 22:07:43 +0800

    chore: setup structures

--- basicsC/linklist/linklist.c
+++ basicsC/linklist/linklist.c
@@ -1,8 +1,23 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+typedef struct song_tag
+{
+    char title[20];
+    char artist[20];
+    char album[20];
+    struct song_tag *nextSong;
+} song;
+typedef struct playlist_tag
+{
+    char name[50];
+    int songCount;
+    song *songHead;
+    struct playlist_tag *next;
+} playlist;
 
-int main(){
-    
+int main()
+{
+
     return 0;
 }
