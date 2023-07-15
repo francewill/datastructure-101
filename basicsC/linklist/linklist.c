fcf4629b449ed129d09473d28727fb4a4ea3e045
francewill <franceureta7@gmail.com>
2023-07-15 22:43:47 +0800

    feat: create main menu

--- basicsC/linklist/linklist.c
+++ basicsC/linklist/linklist.c
@@ -16,8 +16,48 @@ typedef struct playlist_tag
     struct playlist_tag *next;
 } playlist;
 
+char menu();
+
 int main()
 {
-
+    char choice = menu();
+    switch (choice)
+    {
+    case '1':
+        printf("\nThis is option 1");
+        break;
+    case '2':
+        printf("\nThis is option 2");
+        break;
+    case '3':
+        printf("\nThis is option 3");
+        break;
+    case '4':
+        printf("\nThis is option 4");
+        break;
+    case '5':
+        printf("\nThis is option 5");
+        break;
+    case '6':
+        printf("\nThis is option 6");
+        exit(0);
+    default:
+        printf("\nInvalid choice!\n");
+    }
     return 0;
+}
+
+char menu()
+{
+    char choice;
+    printf("\n======= MENU =======");
+    printf("\n[1] Add Playlist");
+    printf("\n[2] Add Song to Playlist");
+    printf("\n[3] Remove Song from Playlist");
+    printf("\n[4] View a Playlist");
+    printf("\n[5] View All Data");
+    printf("\n[6] Exit");
+    printf("\n\nEnter your choice: ");
+    scanf(" %c", &choice);
+    return choice;
 }
