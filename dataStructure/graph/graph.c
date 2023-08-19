5b4ec23954a12bb702cf2966311d3b48ca91e8c8
francewill <franceureta7@gmail.com>
2023-08-19 21:20:03 +0800

    set up stack functions

--- dataStructure/graph/graph.c
+++ dataStructure/graph/graph.c
@@ -52,12 +52,79 @@ int *createVisited(GRAPH *G){
 
 }
 
+// Stack functions
+/*
+** printStack()
+** requirements: none
+** results:
+	if list is empty, prints "*empty*"
+	otherwise, prints the contents of a list
+*/
+void printStack(LIST *L);
+
+
+
+/*
+** createNode()
+** requirements: an integer data
+** results:
+	creates an empty node with value `data`
+	initializes fields of the structure
+	returns the created node
+*/
+NODE* createNode(int data);
+
+
+
+/*
+** createStack()
+** requirements: none
+** results:
+	creates an empty list
+	initializes `head` field of the structure
+	returns the created list
+*/
+LIST* createStack();
+
+
+
+/*
+** isEmpty()
+** requirements: none
+** results:
+	returns 1 if the list is empty
+	otherwise returns 0
+*/
+int isEmpty(LIST *L);
+
+
+
+/*
+** push()
+** requirements: a list and a node to be inserted
+** results:
+	inserts `node` before the current `head` of the list
+*/
+void push(LIST *L, NODE* node);
+
+
+
+/*
+** pop()
+** requirements: a list that must not be empty
+** results:
+	deletes the `head` node of the list
+	returns the value of the deleted node
+*/
+int pop(LIST *L);
+
 /*
 	DFS
 	- implements a non-recursive DFS traversal
 	- you may use the functions in the stack.h file 
 */
 void dfs(GRAPH *G, int start){
+    int *visited = createVisited(G);
 
 }
 
