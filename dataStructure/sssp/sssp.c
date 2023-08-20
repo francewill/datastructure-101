01a9d22ea30bd35641d55feb189a859c5e45296a
francewill <franceureta7@gmail.com>
2023-08-20 15:15:27 +0800

    chore: setup sssp

---
+++ dataStructure/sssp/sssp.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "graph.h"
+
+//Implement your functions here
+
+
+int main() {
+	char command;
+	int vertices, lines, u, v, w, source, target;
+
+	scanf("%d", &vertices);
+	GRAPH *G = createGraph(vertices);
+
+	while(1) {
+		scanf(" %c", &command);
+
+		switch(command) {
+			case '+':
+				scanf(" %d %d %d", &u, &v, &w);
+				insertEdge(G, u-1, v-1, w); //there's a -1 since we use 0-indexing in the arrays
+				printf("Successfully inserted edge %d %d\n", u, v);
+				break;
+			case '#':
+				scanf(" %d %d", &source, &target);
+				dijkstra(G, source-1, target-1);
+				printf("\n");
+				break;
+			case 'p':
+				printf("\nADJACENCY MATRIX: \n");
+				printMatrix(G);
+				break;
+			case 'Q':
+				freeMatrix(G);
+				free(G);
+				return 0;
+			default:
+				printf("Unknown command: %c\n", command);
+		}
+	}
+}
