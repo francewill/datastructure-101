f665b9b4c1547acf198776e190816e521d465044
francewill <franceureta7@gmail.com>
2023-08-23 01:34:21 +0800

    chore: proper indentation

--- dataStructure/sssp/sssp.c
+++ dataStructure/sssp/sssp.c
@@ -2,37 +2,39 @@
 #include <stdlib.h>
 #include "graph.h"
 
-
 /*
 	CREATE GRAPH
 	- allocates memory for the graph
 	- initializes the members of the structure + arrays
 	- returns the newly created graph
 */
-GRAPH *createGraph(int vertices){
-	GRAPH *G = (GRAPH*) malloc (sizeof(GRAPH));
+GRAPH *createGraph(int vertices)
+{
+	GRAPH *G = (GRAPH *)malloc(sizeof(GRAPH));
 	G->num_vertices = vertices;
-	G->matrix = (int**) malloc(sizeof(int*)*vertices);
-	for(int i =0;i<G->num_vertices;i++){
-		G->matrix[i] = (int*) malloc(sizeof(int)*vertices);
+	G->matrix = (int **)malloc(sizeof(int *) * vertices);
+	for (int i = 0; i < G->num_vertices; i++)
+	{
+		G->matrix[i] = (int *)malloc(sizeof(int) * vertices);
 	}
-	for(int i = 0;i<G->num_vertices;i++){
-		for(int j = 0; j<G->num_vertices;j++){
+	for (int i = 0; i < G->num_vertices; i++)
+	{
+		for (int j = 0; j < G->num_vertices; j++)
+		{
 			G->matrix[i][j] = 0;
 		}
 	}
 	return G;
 }
 
-
 /*
 	INSERT EDGE
 	- plots the adjacent vertices in the adjacency matrix
 */
-void insertEdge(GRAPH *G, int u, int v, int w){
+void insertEdge(GRAPH *G, int u, int v, int w)
+{
 	G->matrix[u][v] = w;
 }
-
 
 /*
 	CREATE VISITED
@@ -40,14 +42,15 @@ void insertEdge(GRAPH *G, int u, int v, int w){
 	- this will be used in the dijkstra() function to mark visited nodes
 	- 1: visited, 0: not visited yet
 */
-int *createVisited(GRAPH *G){
-	int *visited = (int*) malloc (sizeof(int)*G->num_vertices);
-	for(int i = 0; i< G->num_vertices;i++){
+int *createVisited(GRAPH *G)
+{
+	int *visited = (int *)malloc(sizeof(int) * G->num_vertices);
+	for (int i = 0; i < G->num_vertices; i++)
+	{
 		visited[i] = 0;
 	}
 	return visited;
 }
-
 
 /*
 	CREATE DISTANCE
@@ -55,14 +58,15 @@ int *createVisited(GRAPH *G){
 	- this will be used in the dijkstra() function to keep track of the distance of the vertices
 	- initialize to 99999 (maximum distance possible)
 */
-int *createDistance(GRAPH *G){
-	int *distance = (int*)  malloc (sizeof(int)*G->num_vertices);
-	for(int i = 0; i< G->num_vertices;i++){
+int *createDistance(GRAPH *G)
+{
+	int *distance = (int *)malloc(sizeof(int) * G->num_vertices);
+	for (int i = 0; i < G->num_vertices; i++)
+	{
 		distance[i] = 99999;
 	}
 	return distance;
 }
-
 
 /*
 	CREATE PARENT
@@ -70,14 +74,15 @@ int *createDistance(GRAPH *G){
 	- this will be used in the dijisktra() function to keep track of the parent of a visited vertex
 	- initialize to -1 (no parent yet)
 */
-int *createParent(GRAPH *G){
-	int *parent = (int *) malloc (sizeof(int)*G->num_vertices);
-	for(int i = 0; i<G->num_vertices;i++){
+int *createParent(GRAPH *G)
+{
+	int *parent = (int *)malloc(sizeof(int) * G->num_vertices);
+	for (int i = 0; i < G->num_vertices; i++)
+	{
 		parent[i] = 0;
 	}
 	return parent;
 }
-
 
 /*
 	DIJKSTRA
@@ -85,61 +90,69 @@ int *createParent(GRAPH *G){
 	- computes for the distance of the path
 	- you may create additional functions for the different operations needed in the dijkstra's algorithm
 */
-void dijkstra(GRAPH *G, int source, int target);
+void dijkstra(GRAPH *G, int source, int target)
+{
+}
 
 /*
 	PRINT MATRIX
 	- prints the values of the adjacency matrix
 	- follow the layout provided in the sample run
 */
-void printMatrix(GRAPH *G){
-	for(int i = 0; i<G->num_vertices;i++){
-		for(int j = 0; j<G->num_vertices;j++){
-			printf("%d     ",G->matrix[i][j]);
+void printMatrix(GRAPH *G)
+{
+	for (int i = 0; i < G->num_vertices; i++)
+	{
+		for (int j = 0; j < G->num_vertices; j++)
+		{
+			printf("%d     ", G->matrix[i][j]);
 		}
 		printf("\n");
 	}
 }
 
-
 /*
 	FREE MATRIX
 	- frees the allocated memory for the adjacency matrix
 */
-void freeMatrix(GRAPH *G);
+void freeMatrix(GRAPH *G)
+{
+}
 
-
-int main() {
+int main()
+{
 	char command;
 	int vertices, lines, u, v, w, source, target;
 
 	scanf("%d", &vertices);
 	GRAPH *G = createGraph(vertices);
 
-	while(1) {
+	while (1)
+	{
 		scanf(" %c", &command);
 
-		switch(command) {
-			case '+':
-				scanf(" %d %d %d", &u, &v, &w);
-				insertEdge(G, u-1, v-1, w); //there's a -1 since we use 0-indexing in the arrays
-				printf("Successfully inserted edge %d %d\n", u, v);
-				break;
-			case '#':
-				scanf(" %d %d", &source, &target);
-				dijkstra(G, source-1, target-1);
-				printf("\n");
-				break;
-			case 'p':
-				printf("\nADJACENCY MATRIX: \n");
-				printMatrix(G);
-				break;
-			case 'Q':
-				freeMatrix(G);
-				free(G);
-				return 0;
-			default:
-				printf("Unknown command: %c\n", command);
+		switch (command)
+		{
+		case '+':
+			scanf(" %d %d %d", &u, &v, &w);
+			insertEdge(G, u - 1, v - 1, w); // there's a -1 since we use 0-indexing in the arrays
+			printf("Successfully inserted edge %d %d\n", u, v);
+			break;
+		case '#':
+			scanf(" %d %d", &source, &target);
+			dijkstra(G, source - 1, target - 1);
+			printf("\n");
+			break;
+		case 'p':
+			printf("\nADJACENCY MATRIX: \n");
+			printMatrix(G);
+			break;
+		case 'Q':
+			freeMatrix(G);
+			free(G);
+			return 0;
+		default:
+			printf("Unknown command: %c\n", command);
 		}
 	}
 }
