34f105e4bc833e6f0a0cb68d0406a6332dc9a761
francewill <franceureta7@gmail.com>
2023-09-03 23:22:04 +0800

    docs: document your code

--- sorting/quicksort.c
+++ sorting/quicksort.c
@@ -1,42 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int partition(int *array, int start, int end)
+int partition(int *array, int start, int end)  // this will divide the array and responsible for swapping
 {
     int pivot = array[start], i = start, j = end, temp;
-    while (i < j)
+    while (i < j)  // loop until i does not pass j
     {
         do
         {
-            i++;
+            i++; // increment i until  we found a number lesser than pivot
         } while (array[i] <= pivot);
         do
         {
-            j--;
+            j--;  // decrement until we found a  number greater than pivot
         } while (array[j] > pivot);
 
-        if (i < j)
+        if (i < j)  // check if i is less than j
         {
-            temp = array[i];
+            temp = array[i];  // swap the values
             array[i] = array[j];
             array[j] = temp;
         }
     }
-    temp = pivot;
+    temp = pivot;  // after the loop swap the pivot and the j
     array[start] = array[j];
     array[j] = temp;
     return j;
 }
 
-void quicksortRecursion(int *array, int start, int end)
+void quicksortRecursion(int *array, int start, int end)  // recursion for quicksort
 {
     int helper;
-    if (start < end)
+    if (start < end)  // always check if we
     {
 
-        helper = partition(array, start, end);
-        quicksortRecursion(array, start, helper);
-        quicksortRecursion(array, helper + 1, end);
+        helper = partition(array, start, end); // divide the array
+        quicksortRecursion(array, start, helper);  // this is for the left side
+        quicksortRecursion(array, helper + 1, end);  // this is for the right side
     }
 }
 
@@ -59,10 +59,13 @@ int main()
     array[7] = 9;
     array[8] = 5;
     array[9] = 99999;
+    
     // for( int i = 0; i< 9; i++){
     //     array[i] = rand() % (99 + 1 - 0) + 0;
     // }
-    quicksort(array, 9);
+
+    quicksort(array, 9);  // call the quicksort function
+
     for (int i = 0; i < 10; i++)
     {
         printf("%d ", array[i]);
