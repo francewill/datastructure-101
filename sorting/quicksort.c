59dbde3e1deeda875eaa19c994f52f1b99a32e4a
francewill <franceureta7@gmail.com>
2023-09-03 23:23:19 +0800

    chore: add random numbers

--- sorting/quicksort.c
+++ sorting/quicksort.c
@@ -49,20 +49,11 @@ int main()
 {
 
     int *array = (int *)malloc(sizeof(int) * 10);
-    array[0] = 10;
-    array[1] = 16;
-    array[2] = 8;
-    array[3] = 12;
-    array[4] = 15;
-    array[5] = 6;
-    array[6] = 3;
-    array[7] = 9;
-    array[8] = 5;
-    array[9] = 99999;
-    
-    // for( int i = 0; i< 9; i++){
-    //     array[i] = rand() % (99 + 1 - 0) + 0;
-    // }
+
+
+    for( int i = 0; i< 9; i++){
+        array[i] = rand() % (99 + 1 - 0) + 0;
+    }
 
     quicksort(array, 9);  // call the quicksort function
 
