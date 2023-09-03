c58a6ccaebb83f47206fc44dfc3ce3d239fa9253
francewill <franceureta7@gmail.com>
2023-09-03 15:34:34 +0800

    feat: create a random numbers

--- sorting/quicksort.c
+++ sorting/quicksort.c
@@ -2,5 +2,15 @@
 #include<stdlib.h>
 
 int main(){
+
+    int *array = (int *) malloc (sizeof(int)*10);
+    array[9] = 99999;
+    for( int i = 0; i< 10; i++){
+        array[i] = rand() % (99 + 1 - 0) + 0; 
+    }
+
+    for(int i = 0; i < 10; i++){
+        printf("%d ", array[i]);
+    }
     return 0;
 }
