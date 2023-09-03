bc9fb7b94d6176f7e03ee4e82172d7108a238182
francewill <franceureta7@gmail.com>
2023-09-03 16:14:28 +0800

    feat: create partition function

--- sorting/quicksort.c
+++ sorting/quicksort.c
@@ -1,13 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
+int partition(int *array, int start, int end){
+    int pivot = array[start], i = start, j= end, temp;
+    while(i<j){
+        do{
+            i++;
+        }while(array[i]>pivot);
+        do{
+            j++;
+        }while(array[j]<pivot);
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    temp = pivot; 
+    pivot = array[j];
+    array[j] = temp;
 
+    
+}
 int main(){
 
     int *array = (int *) malloc (sizeof(int)*10);
+    array[0] = 10;
+    array[1] = 16;
+    array[2] = 8;
+    array[3] = 12;
+    array[4] = 15;
+    array[5] = 6;
+    array[6] = 3;
+    array[7] = 9;
+    array[8] = 5;
     array[9] = 99999;
-    for( int i = 0; i< 10; i++){
-        array[i] = rand() % (99 + 1 - 0) + 0; 
-    }
+    // for( int i = 0; i< 9; i++){
+    //     array[i] = rand() % (99 + 1 - 0) + 0; 
+    // }
 
     for(int i = 0; i < 10; i++){
         printf("%d ", array[i]);
